43f9e0008a3f09a69db4d7a58284e382a0b52428
zhang hailong <hailongz@qq.com>
2013-06-20 19:08:58 +0800

    add

---
+++ vMessageServer/vMessageServer/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  vMessageServer
+//
+//  Created by zhang hailong on 13-6-20.
+//  Copyright (c) 2013年 hailong.org. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
